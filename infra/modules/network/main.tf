692ce4971fd38666d1f744a1090ab0ef2e41ff07
EERAH-LEE <kor.si1229@gmail.com>
2026-06-15 13:15:17 +0900

    0615/eerah aws인프라 초안

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,19 +1,19 @@
 resource "aws_vpc" "main" {
-    cidr_block = local.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostnames = true
+  cidr_block           = local.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-      Name = "vpc-${var.namespace}"
-    }
+  tags = {
+    Name = "vpc-${var.namespace}"
+  }
 }
 
 # EKS 노드용 프라이빗 서브넷
 resource "aws_subnet" "eks" {
-  count = 2   # 서브넷 2개 만들기
-  vpc_id = aws_vpc.main.id  # 위에 만든 vpc에 연결
-  cidr_block = local.eks_subnet_cidrs[count.index] #[0],[1]로 구분
-  availability_zone = local.AZs[count.index] #[0]=2a존, [1]=2c존
+  count             = 2                                   # 서브넷 2개 만들기
+  vpc_id            = aws_vpc.main.id                     # 위에 만든 vpc에 연결
+  cidr_block        = local.eks_subnet_cidrs[count.index] #[0],[1]로 구분
+  availability_zone = local.AZs[count.index]              #[0]=2a존, [1]=2c존
 
   tags = {
     Name = "subnet-${var.namespace}-eks-${count.index + 1}"
@@ -22,9 +22,9 @@ resource "aws_subnet" "eks" {
 
 # RDS용 프라이빗 서브넷 - RDS는 서브넷그룹에 최소 2개 AZ 필요
 resource "aws_subnet" "rds" {
-  count = 2 
-  vpc_id = aws_vpc.main.id
-  cidr_block = local.rds_subnet_cidrs[count.index]
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.rds_subnet_cidrs[count.index]
   availability_zone = local.AZs[count.index]
 
   tags = {
@@ -38,7 +38,7 @@ resource "aws_internet_gateway" "main" {
 
   tags = {
     Name = "igw-${var.namespace}"
-  }  
+  }
 }
 
 #Public rt
@@ -46,9 +46,9 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"   # 모든 트래픽을 IGW로
+    cidr_block = "0.0.0.0/0" # 모든 트래픽을 IGW로
     gateway_id = aws_internet_gateway.main.id
-  }  
+  }
 
   tags = {
     Name = "rt-${var.namespace}-public"
