98c66500902a824e8896bfb4dc36fc6db1d48a12
EERAH-LEE <kor.si1229@gmail.com>
2026-06-17 14:32:19 +0900

    0617/eerah add:dr-trigger, delete route53

--- infra/core/variables.tf
+++ infra/core/variables.tf
@@ -37,3 +37,12 @@ variable "azure_vnet_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+variable "azure_agw_fqdn" {
+  description = "Azure AGW FQDN 또는 공인 IP"
+  type        = string
+}
+
+variable "slack_webhook_url" {
+  type      = string
+  sensitive = true
+}
