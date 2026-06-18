bf6d4eb6ad999433c12dd1ef6f7e1f92cd2eb570
EERAH-LEE <kor.si1229@gmail.com>
2026-06-18 17:11:25 +0900

    0618/eerah add: DMS 역방향

--- infra/core/modules/rds/main.tf
+++ infra/core/modules/rds/main.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "main" {
   identifier = "mysql-${var.namespace}"
   engine = "mysql"
   engine_version = "8.0"
-  instance_class = "db.t3.micro"
+  instance_class = "db.t3.small"
   allocated_storage = 20
 
   db_name = var.db_name
@@ -24,8 +24,38 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids = [var.rds_sg_id]
 
   skip_final_snapshot = true #테스트용, 삭제 시 스냅샷 안찍음
+  parameter_group_name = aws_db_parameter_group.main.name
+
 
   tags = {
     Name = "mysql-${var.namespace}"
+  }
+}
+
+
+####역뱡향용####
+# RDS 파라미터 그룹 - DMS 소스로 사용하기 위한 binlog 설정
+resource "aws_db_parameter_group" "main" {
+  name   = "pg-${var.namespace}"
+  family = "mysql8.0"
+
+  # 커넥션 수 제한 증가 (DMS + 앱 동시 접속 대비)
+  parameter {
+    name  = "max_connections"
+    value = "200"
+  }
+
+  # DMS CDC가 읽을 수 있는 바이너리 로그 형식 (ROW 필수)
+  parameter {
+    name  = "binlog_format"
+    value = "ROW"
+    apply_method = "pending-reboot"
+  }
+
+  # DMS binlog 읽기 시 체크섬 검증 비활성화
+  parameter {
+    name  = "binlog_checksum"
+    value = "NONE"
+    apply_method = "pending-reboot"
   }
 }
